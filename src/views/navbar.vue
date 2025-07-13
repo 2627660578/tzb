603f0fa61ef794094c62ab1cd68c709e18f22ab3
Alice <2627660578.@qq.com>
2025-07-13 22:35:16 +0800

    上传

--- src/views/navbar.vue
+++ src/views/navbar.vue
@@ -1,7 +1,6 @@
 <template>
   <nav class="navbar">
     <img src="../assets/vue.svg" alt="LOGO">
-
     <ul class="font">
       <li>
         <router-link to="/">首页</router-link>
@@ -16,12 +15,25 @@
         <router-link to="/self">个人中心</router-link>
       </li>
     </ul>
+
+    <button v-if="mainStore.isLoggedin" @click="handleLogout" class="button">退出登录</button>
   </nav>
 </template>
 
 <script setup lang="ts">
 import { useMainStore } from '../stores/message'
+import { useRouter } from 'vue-router' // 2. 导入 useRouter
 const mainStore = useMainStore()
+
+const router = useRouter() // 3. 获取 router 实例
+
+// 4. 创建一个处理登出的方法
+const handleLogout = () => {
+  // 调用 store 中的 logout action
+  mainStore.logout()
+  // 登出后跳转到登录页面
+  router.push('/login')
+}
 </script>
 
 <style scoped>
@@ -50,9 +62,7 @@ img {
   width: 100%;
   height: 100%;
   background-color: white;
-  color: white;
   box-shadow: 0 5px 4px rgba(0, 0, 0, 0.1);
-  z-index: 1000;
   align-items: center;
   /* justify-content: center; */
 }
@@ -77,4 +87,22 @@ img {
   color: #007bff;
   transform: scale(1.15);
 }
+
+.button {
+    align-items: center;
+    /* background-color: #437faa; */
+    width: 120px;
+    color: black;
+    border: none;
+    border-width: 1px;
+    font-size: 15px;
+    letter-spacing: 2px;
+    margin-right: 10px;
+    /* margin-top: 30px; */
+    padding: 10px 10px;
+    border-radius: 5px;
+    cursor: pointer;
+
+}
+
 </style>
